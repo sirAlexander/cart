2824e8e75027a2a07066624ff5d6976351f9f588
Alexander TIndi <>
2019-06-01 18:08:38 +0100

    Initial commit

---
+++ src/main/java/com/shikanga/cart/controllers/CartController.java
@@ -0,0 +1,35 @@
+package com.shikanga.cart.controllers;
+
+import com.shikanga.cart.model.Cart;
+import com.shikanga.cart.model.Product;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("v1/cart")
+public class CartController {
+
+    @GetMapping("/{userId}")
+    public Cart getCart(@Valid @PathVariable long userId) {
+
+        List<Product> products = new ArrayList<>();
+
+        Product p1 = new Product(1, "keyboard", 250, 2);
+        p1.setTotalPrice(p1.getBasePrice() * p1.getQuantity());
+        products.add(p1);
+
+        Product p2 = new Product(2, "mouse", 150, 2);
+        p2.setTotalPrice(p2.getBasePrice() * p2.getQuantity());
+        products.add(p2);
+
+        double totalPrice = products.stream().mapToDouble(Product::getTotalPrice).sum();
+
+        return new Cart(products.size(), totalPrice, products);
+    }
+}
---
+++ src/main/java/com/shikanga/cart/model/Cart.java
@@ -0,0 +1,40 @@
+package com.shikanga.cart.model;
+
+import java.util.List;
+
+public class Cart {
+
+    private int totalItems;
+    private double totalPrice;
+    private List<Product> products;
+
+    public Cart(int totalItems, double totalPrice, List<Product> products) {
+        this.totalItems = totalItems;
+        this.totalPrice = totalPrice;
+        this.products = products;
+    }
+
+    public int getTotalItems() {
+        return totalItems;
+    }
+
+    public void setTotalItems(int totalItems) {
+        this.totalItems = totalItems;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+}
